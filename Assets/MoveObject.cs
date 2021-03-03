ada6e60dd28d251f796ea8b37e11943f4c59351a
dabandabanda <kristaps.loc@gmail.com>
2021-03-03 12:31:13 +0000

    Session 3 - changes 2

---
+++ Assets/MoveObject.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveObject : MonoBehaviour
+{
+
+    Vector3 targetPosition;
+    public LayerMask ObjectToSelectLayer;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (Input.GetMouseButton(0))
+        {
+            CastRay();
+        }
+
+    }
+
+    void CastRay()
+    {
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit2D hit = Physics2D.Raycast(ray.origin, ray.direction, Mathf.Infinity, ObjectToSelectLayer);
+        if (hit)
+        {
+
+        }
+    }
+}
